bbfc15a295c13eafdc18fc3b52fa7c311b0bdfc0
aleksandra-kim <aleksandra.kim@icloud.com>
2022-01-03 17:51:53 +0100

    added allocation by production volume

--- bigfood_db/utils.py
+++ bigfood_db/utils.py
@@ -3,7 +3,7 @@ from bw2io.utils import rescale_exchange
 from copy import deepcopy
 
 
-def create_location_mapping(db1, db2_name):
+def create_location_mapping_glo(db1, db2_name):
     """
     Create mapping between unlinked exchanges of database `db1` and their corresponding activities in `db2`.
     Here an exchange in `db1` needs to be linked to activity in `db2` with location `GLO`, which does not exist there.
@@ -11,163 +11,133 @@ def create_location_mapping(db1, db2_name):
     For example, CH + DE + RoW = GLO, where Rest of the World (RoW) basically adds whatever locations are missing to
     result in GLO. Hence, the mapping is from unlinked exchanges of `db1` and list of activities in `db2` with same name
     but multiple locations.
-    Example: (market for lime, GLO) is split by production volume into
+    Example: (market for lime, GLO) can be later split by production volume into
              (market for lime, RoW) & (market for lime, RER).
     Mapping is a list of dictionaries where each dictionary corresponds to an unlinked exchange.
-    The key is the index of the unlinked exchange in `db`` and the values are `db2` activities codes.
+    The key is the (name, location) tuple of the unlinked exchange in `db`` and the values are `db2` activities.
+
     """
+
     db2 = bd.Database(db2_name)
     unlinked_list = [act for act in db1.unlinked if act['type'] == 'technosphere' and act.get('location') == "GLO"]
-    mapping = [0]*len(unlinked_list)
+    mapping = []
     for i, activity in enumerate(unlinked_list):
         new_el = {}
         name = activity['name']
         loc = activity['location']
-        acts_codes = [act['code'] for act in db2 if name == act['name']]
-        new_el[(name, loc)] = acts_codes
-        mapping[i] = new_el
+        acts_codes = [(db2_name, act['code']) for act in db2 if name == act['name']]
+        if len(acts_codes) > 0:
+            new_el[(name, loc)] = acts_codes
+            mapping.append(new_el)
     return mapping
 
 
-def get_allocated_excs(db1, mapping, db2_name):
+def create_location_mapping_rer(db1, db2_name):
     """
-    Function that generates list of exchanges with
-    Each "inner" list contains dictionaries of exchanges in the correct format.
-    By correct format we mean the one consistent with the format of exchanges
-    in the (not written yet) consumption database (see eg co.data[0]['exchanges']).
-    We exclude amounts for now and instead add field 'production volume share'
-
-    Attributes
-    ----------
-    db1 : object
-        Brightway database
-    mapping : list of dictionaries
-        Each dictionary corresponds to an unlinked exchange with
-            key = index of the unlinked exchange in `list(db1.unlinked)`
-            values = codes of corresponding activities
-    db2_name : name of the database to link to
-
-    Returns
-    -------
-    unlinked_list_used : list
-        List of unlinked exchanges that are actually present in the mapping.
-    allocated_excs : list of lists
-        Each inner list contains exchanges dictionaries
+    Create mapping between unlinked exchanges of database `db1` and their corresponding activities in `db2`.
+    Here an exchange in `db1` needs to be linked to activity in `db2` with location `RER` (Europe), which does not exist
+    there. Instead, there are 2 activities with the same name but locations `Europe without XX` and `XX`, where
+    XX is some country. Locations can also be given as `RER w/o XX` and `X`.
+    For example, Europe without Austria + AT = RER (Europe). Hence, the mapping is from unlinked exchanges of `db1`
+    and list of activities in `db2` with same name and 2 locations.
+    Example: (market for lime, RER) can be later split by production volume into
+             (market for lime, AT) & (market for lime, Europe without Austria).
+    Mapping is a list of dictionaries where each dictionary corresponds to an unlinked exchange.
+    The key is the (name, location) tuple of the unlinked exchange in `db`` and the values are `db2` activities.
 
     """
 
-    unlinked_list = [act for act in db1.unlinked if act['type'] == 'technosphere' and act.get('location')]
-    len_unlinked = len(unlinked_list)
-
-    unlinked_names_loc = [0]*len_unlinked
-    for i in range(len_unlinked):
-        unlinked_names_loc[i] = (unlinked_list[i]['name'], unlinked_list[i]['location'])
-
-    unlinked_list_used = []
-    allocated_excs = []
-
-    for m in range(len(mapping)):
-        try:
-            # If current element from mapping is in unlinked exchanges, save it in `unlinked_list_used`
-            index = unlinked_names_loc.index(list(mapping[m].keys())[0])
-            unlinked_list_used.append(unlinked_list[index])
-
-            # Change exchanges of the current activity if some of them are unlinked.
-            # Involves adding new allocation exchanges to `allocated_excs` and adding field `production volume share`
-            new_exchanges = []
-            vols = 0
-            codes = list(mapping[m].values())[0]
-            for code in codes:
-                act = bd.get_activity((db2_name, code))
-                production_exc = next(item for item in act.exchanges() if item['type'] == 'production')
-                vol = production_exc['production volume']
-                vols += vol
-                exc = deepcopy(unlinked_list[index])
-                # Update some values to be consistent with db_name
-                exc2 = {'name': act['name'],
-                        'reference product': act['reference product'],
-                        'location': act['location'],
-                        'production volume share': vol,
-                        'unit': act['unit'],
-                        'database': db2_name,
-                        'type': 'technosphere'}
-                exc.update(exc2)
-
-                new_exchanges.append(exc)
-
-            for exc in new_exchanges:
-                exc['production volume share'] /= vols
-
-            allocated_excs.append(new_exchanges)
-
-        except ValueError:
-            pass
-
-    return unlinked_list_used, allocated_excs
+    db2 = bd.Database(db2_name)
+    unlinked_list = [act for act in db1.unlinked if act['type'] == 'technosphere' and act.get('location') == "RER"]
+    mapping = []
+    import country_converter as coco
+    for i, activity in enumerate(unlinked_list):
+        new_el = {}
+        name = activity['name']
+        loc = activity['location']
+        for rer_wo_string in ["Europe without ", "RER w/o "]:
+            act_rer_wo_xx = [
+                act for act in db2 if name == act['name'] and rer_wo_string in act['location']
+            ]
+            # if len(act_rer_wo_xx) >= 1:
+            #     print(act_rer_wo_xx)
+            if len(act_rer_wo_xx) == 1:
+                act_rer_wo_xx = act_rer_wo_xx[0]
+                xx_location_long = act_rer_wo_xx['location'].replace(rer_wo_string, "")
+                xx_location_iso2 = coco.convert(names=xx_location_long, to='ISO2')
+                act_xx = [act for act in db2 if name == act['name'] and xx_location_iso2 == act['location']]
+                assert len(act_xx) == 1
+                act_xx = act_xx[0]
+                new_el[(name, loc)] = [
+                    (db2_name, act_rer_wo_xx['code']),
+                    (db2_name, act_xx['code'])
+                ]
+                mapping.append(new_el)
+    return mapping
 
 
-def compare_exchanges(exc1, exc2, db_name):
-    """Function that compares two exchanges based on certain fields. Return True if exchanges are the same."""
+def get_activities_for_allocation(mapping, db2_name):
+    """
+    Function that adds production volume share to activities in the mapping. Needed for allocation.
 
-    # Do not consider biosphere exchanges
-    if exc1['type'] == 'biosphere' or exc2['type'] == 'biosphere':
-        return False
+    """
 
-    # Do not need to consider exchanges that are not in the database we're linking to
-    try:
-        if exc1['input'][0] != db_name:
-            return False
-    except:
-        pass
-
-    # Compare exchanges based on their dictionary fields
-    fields_to_compare = ['name', 'location', 'unit', 'type']
-    same = all([exc1[f] == exc2[f] for f in fields_to_compare])
-
-    return same
+    acitivities_with_allocation = {}
+    for dict_ in mapping:
+        key = list(dict_.keys())[0]
+        mapped_activities = list(dict_.values())[0]
+        total_volume = 0
+        data = []
+        for db_code in mapped_activities:
+            activity = bd.get_activity(db_code)
+            production_exchange = next(item for item in activity.exchanges() if item['type'] == 'production')
+            production_volume = production_exchange['production volume']
+            data.append(
+                {
+                    'name': activity['name'],
+                    'reference product': activity['reference product'],
+                    'location': activity['location'],
+                    'production volume share': production_volume,
+                    'unit': activity['unit'],
+                    'database': db2_name,
+                    'type': 'technosphere'
+                }
+            )
+            total_volume += production_volume
+        #         print(production_volume)
+        #     print(total_volume)
+        #     print()
+        for d in data:
+            d["production volume share"] /= total_volume
+        acitivities_with_allocation[key] = data
+    return acitivities_with_allocation
 
 
 def modify_exchanges(db1, mapping, db2_name):
     """
-    Change exchanges of activities if they are unlinked, adjust their amount based on `production volume share` field.
-    TODO: change the code to removing unlinked exchanges instead of adding them - line 121-...
-    TODO: uncertainty info is not scaled!!!
+    Function that rescales exchanges in `db1.data` based on their production volume shares.
+
     """
 
-    db1 = deepcopy(db1)
-    unlinked_list_used, allocated_excs = get_allocated_excs(db1, mapping, db2_name)
-    for act in db1.data:
-        try:
-            exchanges = deepcopy(act['exchanges'])
-            new_exchanges = []
+    db1_copy = deepcopy(db1)
+    activities_to_allocate = get_activities_for_allocation(mapping, db2_name)
 
-            for exc in exchanges:
-
-                ind = next(
-                    (i for i, item in enumerate(unlinked_list_used) if compare_exchanges(exc, item, db2_name)),
-                    None
-                )
-
-                if ind is not None:
-                    # if we find current exchange in the unlinked exchanges list, replace it with other ones
-                    # while using allocation by production volume
-                    allocated_excs_new_amt = deepcopy(allocated_excs[ind])
-                    # for exc_new_amt in allocated_excs_new_amt:÷
-                    # exc_new_amt['amount'] = exc_new_amt['production volume share'] * exc['amount']
-                    for exc_new_amt in allocated_excs_new_amt:
-                        if 'production volume share' in exc_new_amt:
-                            exc_new_amt['amount'] = exc['amount']
-                            new_exchanges.append(rescale_exchange(exc_new_amt, exc_new_amt['production volume share']))
-
-                else:
-                    # if we don't find current exchange in the unlinked exchanges list, append current to the list
-                    new_exchanges.append(exc)
-
-            act['exchanges'] = new_exchanges
-
-        except:
-            pass
-
-    db1.match_database(db2_name, fields=('name', 'reference product', 'unit', 'location'))
-
+    for i, act in enumerate(db1_copy.data):
+        exchanges = deepcopy(act.get("exchanges", []))
+        modified_exchanges = []
+        for j, exc in enumerate(exchanges):
+            if (exc['name'], exc.get('location', None)) in list(activities_to_allocate.keys()):
+                for act_to_allocate_db1, acts_db2 in activities_to_allocate.items():
+                    if (exc['name'], exc.get('location', None)) == act_to_allocate_db1:
+                        for act_db2 in acts_db2:
+                            modified_exchange = deepcopy(exc)
+                            modified_exchange['location'] = act_db2['location']
+                            modified_exchanges.append(
+                                rescale_exchange(modified_exchange, act_db2['production volume share'])
+                            )
+            else:
+                modified_exchanges.append(exc)
+        # if len(exchanges) != len(modified_exchanges):
+        #     print(i, len(exchanges), len(modified_exchanges))
+        db1.data[i]['exchanges'] = modified_exchanges
     return db1
